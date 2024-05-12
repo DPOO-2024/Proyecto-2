c4052eb3b0b9cd9a3c87b6a0bc85221dd11a4239
Gabs-489 <141871264+Gabs-489@users.noreply.github.com>
2024-05-12 12:50:06 -0500

    Delete src directory

--- src/Consola/ConsolaBase.java
+++
@@ -1,8 +0,0 @@
-package Consola;
-
-import Exceptions.MensajedeErrorException;
-
-public interface ConsolaBase {
-	public void mostrarMenu();
-	public void iniciarSesion() throws MensajedeErrorException;
-}
